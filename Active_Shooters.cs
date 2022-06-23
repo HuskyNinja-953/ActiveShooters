02d1169f6d75e38617a58180342c15996388ac70
HuskyNinja-953 <Husky.953@proton.me>
2022-06-23 15:27:21 -0400

    Added groups of pedhashes that are related so the shooters that spawn arent a totally random assortment.
    Added a couple more places for the shooters to spawn.

--- Active_Shooters.cs
+++ Active_Shooters.cs
@@ -26,7 +26,10 @@ namespace ActiveShooters
             new Vector3(-878.8595f, -858.1266f, 19.12326f), //Chinese Pagota
             new Vector3(1066.351f, -2447.885f, 29.06232f), //Bus Graveyard
             new Vector3(191.7502f, -946.9447f, 30.09192f), //Art Installation
-            new Vector3(96.85612f, -356.3206f, 42.14159f) //Construction Site
+            new Vector3(96.85612f, -356.3206f, 42.14159f), //Construction Site
+            new Vector3(686.346f, 577.8948f, 130.4613f), //Vinewood Bowl Center Stage
+            new Vector3(1078.587f, -689.6733f, 57.61979f), //Mirror Park Island
+            new Vector3(914.6471f, -1527.537f, 30.87168f) //Tractor Parts Warehouse
         };
 
         private Vector3 calloutLocation;
@@ -68,9 +71,12 @@ namespace ActiveShooters
             int shooterCount = rand.Next(2, 6);
 
             try{
+                //Get the pedHash group for the shooters
+                List<PedHash> groupHash = GetGroupHashes();
+
                 //Spawn in the shooters
-                for(int i = 0; i < shooterCount; i++){
-                    Ped shooter = await CreateShooter();
+                for (int i = 0; i < shooterCount; i++){
+                    Ped shooter = await CreateShooter(groupHash);
                     shooters.Add(shooter);
 
                     //20% chance the shooter will attack a random pedestrian
@@ -108,8 +114,8 @@ namespace ActiveShooters
         public override void OnCancelBefore(){
             base.OnCancelBefore();
         }
-        private async Task<Ped> CreateShooter(){
-            Ped shooter = await SpawnPed(RandomUtils.GetRandomPed(), calloutLocation.Around(8f));
+        private async Task<Ped> CreateShooter(List<PedHash> groupHash){
+            Ped shooter = await SpawnPed(groupHash.SelectRandom(), calloutLocation.Around(8f));
 
             shooter.Weapons.Give(weaponList.SelectRandom(), 500, true, true);
             shooter.Armor = 2500;
@@ -125,6 +131,44 @@ namespace ActiveShooters
 
             string dialogue = "~r~Suspect~s~: The ~b~Cops~s~ are here! ~r~Shoot~s~ anyone you see!";
             ShowDialog(dialogue, 5000, 25f);
+        }
+
+        private List<PedHash> GetGroupHashes(){
+            //Each list contains similar ped hashes
+            List<PedHash> ballers = new List<PedHash>(){
+                PedHash.BallaEast01GMY,
+                PedHash.BallaOrig01GMY,
+                PedHash.Ballas01GFY,
+                PedHash.Ballasog,
+                PedHash.BallaSout01GMY
+            };
+            List<PedHash> vagos = new List<PedHash>(){
+                PedHash.MexGoon01GMY,
+                PedHash.MexGoon02GMY,
+                PedHash.MexGoon03GMY,
+                PedHash.Vagos01GFY,
+                PedHash.MexGang01GMY
+            };
+            List<PedHash> fam = new List<PedHash>(){
+                PedHash.Families01GFY,
+                PedHash.Famfor01GMY,
+                PedHash.Famdnf01GMY,
+                PedHash.Famca01GMY,
+                PedHash.RampGang
+            };
+            List<PedHash> pros = new List<PedHash>(){
+                PedHash.Blackops01SMY,
+                PedHash.Blackops02SMY,
+                PedHash.Blackops03SMY,
+                PedHash.Marine01SMM,
+                PedHash.Marine03SMY
+            };
+
+            //This is a list containing all those lists.
+            List<List<PedHash>> listOfPedHashLists = new List<List<PedHash>>() { ballers, vagos, fam, pros };
+
+            //Select a random list of ped hashes and return it
+            return listOfPedHashLists.SelectRandom();
         }
     }
 }
