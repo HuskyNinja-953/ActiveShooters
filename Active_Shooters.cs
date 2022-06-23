9b19c90c3d8e635f36d34f8dadedab446e34e0a5
HuskyNinja-953 <Husky.953@proton.me>
2022-06-23 12:57:21 -0400

    Add project files.

---
+++ Active_Shooters.cs
@@ -0,0 +1,16 @@
+﻿using FivePD.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActiveShooters
+{
+    [CalloutProperties("Active Shooters", "HuskyNinja","1.0")]
+    internal class Active_Shooters : Callout{
+        public Active_Shooters(){
+
+        }
+    }
+}
